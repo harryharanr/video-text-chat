d56e70ff260f9ca406eac5d6117a806dbfaff3b1
Hariharan <harryharanr@gmail.com>
2018-02-02 09:34:43 +0530

    first commit

---
+++ signaler.js
@@ -0,0 +1,103 @@
+var fs = require('fs');
+var express = require('express');
+
+var _static = require('node-static');
+var file = new _static.Server('./static', {
+    cache: false
+});
+
+var app = require('http').createServer(serverCallback);
+
+function serverCallback(request, response) {
+    request.addListener('end', function () {
+        response.setHeader('Access-Control-Allow-Origin', '*');
+        response.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+        file.serve(request, response);
+    }).resume();
+}
+
+var io = require('socket.io').listen(app, {
+    log: true,
+    origins: '*:*'
+});
+
+io.set('transports', [
+    // 'websocket',
+    'xhr-polling',
+    'jsonp-polling'
+]);
+
+var channels = {};
+
+io.sockets.on('connection', function (socket) {
+    var initiatorChannel = '';
+    if (!io.isConnected) {
+        io.isConnected = true;
+    }
+
+    socket.on('new-channel', function (data) {
+        if (!channels[data.channel]) {
+            initiatorChannel = data.channel;
+        }
+
+        channels[data.channel] = data.channel;
+        onNewNamespace(data.channel, data.sender);
+    });
+
+    socket.on('presence', function (channel) {
+        var isChannelPresent = !! channels[channel];
+        socket.emit('presence', isChannelPresent);
+    });
+
+    socket.on('disconnect', function (channel) {
+        if (initiatorChannel) {
+            delete channels[initiatorChannel];
+        }
+    });
+
+    // Handle chat event
+    socket.on('chat', function(data){
+        console.log('test');
+        console.log('test');
+        console.log('test');
+        console.log('test');
+        console.log('test');
+        console.log('test');
+        console.log('test');
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+});
+
+function onNewNamespace(channel, sender) {
+    io.of('/' + channel).on('connection', function (socket) {
+        var username;
+        if (io.isConnected) {
+            io.isConnected = false;
+            socket.emit('connect', true);
+        }
+
+        socket.on('message', function (data) {
+            if (data.sender == sender) {
+                if(!username) username = data.data.sender;
+                
+                socket.broadcast.emit('message', data.data);
+            }
+        });
+        
+        socket.on('disconnect', function() {
+            if(username) {
+                socket.broadcast.emit('user-left', username);
+                username = null;
+            }
+        });
+    });
+}
+
+app.listen(8888);
